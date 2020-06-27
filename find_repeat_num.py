5da3efd1611bd3c1ed0b0bf8413da6e5efc212c8
Administrator <2084267015@qq.com>
2020-06-27 20:34:01 +0800

    d

---
+++ find_repeat_num.py
@@ -0,0 +1,21 @@
+class FindRepeatNum:
+    def findRepeatNumber(self, nums: [int]) -> int:
+        i = 0
+        nLen = len(nums)
+        while i < nLen:
+            if nums[i] == i:
+                i += 1
+                continue
+            if nums[i] == nums[nums[i]]:
+                return nums[i]
+            nums[i], nums[nums[i]] = nums[nums[i]], nums[i]
+
+        return -1
+
+    def f2(self, nums: [int]) -> int:
+        s = set()
+        for n in nums:
+            if n in s:
+                return n
+            else:
+                s.add(n)
